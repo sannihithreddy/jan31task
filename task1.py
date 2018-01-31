8cdce5acc19aa1452d548811cf17e992d107762e
sannihithreddy <kamireddy.sannihith@dal.ca>
2018-01-31 09:47:47 -0400

    classroom example code

--- task1.py
+++ task1.py
@@ -1,1 +1,13 @@
-print("sample program")
+import math
+import string
+def __init__(self,oct_a,oct_b,oct_c,oct_d):
+     self.a=oct_a
+     self.b=oct_b
+     self.c=oct_c
+     self.d=oct_d
+     self.netmask=subnet
+def __str__():
+     return(str(self.a)+"."+...)
+def get_broadcast(self):
+    result= my_ipad(self.a&subnet.a,self.b&subnet.b)
+Myadd_host1=my_ipad(10,2,3,4,24)
